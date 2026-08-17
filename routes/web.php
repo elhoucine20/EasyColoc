52202fd6a27aff63e8fd0c7029ffe2406c70967d
elhoucine-naitbrahim <elhoucinenaitbrahim@gmail.com>
2026-08-17 17:41:25 +0100

    Redirect home route to login

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::redirect('/', '/login');
 
 
 Route::get('/register', [registerController::class, 'index'])->name('register');
